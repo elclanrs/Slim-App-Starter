2be96caa0f89c543fe00494c92fbd3f331cebe96
elclanrs <elclanrs@gmail.com>
2014-10-30 03:42:54 -0400

    ...

--- index.php
+++ index.php
@@ -3,12 +3,22 @@
 require 'vendor/autoload.php';
 
 $app = new \Slim\Slim([
-  'templates.path' => 'app/views'
+  'view' => new \Slim\Views\Twig(),
+  'templates.path' => dirname(__FILE__) .'/app/views'
 ]);
+
+$view = $app->view();
+$view->parserExtensions = [
+  new \Slim\Views\TwigExtension()
+];
+$view->parserOptions = [
+  'debug' => true,
+  'cache' => dirname(__FILE__) .'/cache'
+];
 
 $app->get('/', function() use ($app) {
   $data = ['title' => 'Hello World'];
-  $app->render('index.php', $data);
+  $app->render('index.twig', $data);
 });
 
 $app->run();
